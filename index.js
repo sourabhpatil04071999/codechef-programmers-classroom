a01f9045be4608862f941874a8399868f57907b7
Vansh <tiwari1998@hotmail.com>
2020-12-16 08:20:43 +0530

    Again first commit with authentication only

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express();
+var bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+const port = 8000;
+
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.use("/static", express.static("static"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// const csrfMiddleware = csrf({
+//     cookie: true
+// });
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.json());
+
+// app.use(csrfMiddleware);
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./getmyuser-firebase-adminsdk-3dxo8-67dd69ce9b.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://getmyuser.firebaseio.com"
+});
+
+function isAuthenticated(req, res, next){
+    //Check if user is logged in
+    //If they are, get him access
+    //If not return an error...
+
+}
+
+app.get('/auth', (req, res) => {
+    res.render('auth.ejs');
+});
+
+app.get('/fillprofile', (req, res) => {
+    res.render('fillprofile.ejs');
+});
+
+app.get('/teacherDashboard', (req, res) => {
+    res.render('teacherDashboard.ejs');
+});
+
+app.get('/studentDashboard', (req, res) => {
+    res.render('studentDashboard.ejs');
+});
+
+
+app.listen(process.env.port || 3000);
---
+++ static/js/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCX7fqcw2UD0_4N-jfsiP7O4-XBtPXp9g8",
+    authDomain: "getmyuser.firebaseapp.com",
+    databaseURL: "https://getmyuser.firebaseio.com",
+    projectId: "getmyuser",
+    storageBucket: "getmyuser.appspot.com",
+    messagingSenderId: "236793147062",
+    appId: "1:236793147062:web:f6a21cddbe4cf73dbe8017"
+};
